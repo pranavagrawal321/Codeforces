1448f4b617ff68f9440b2734c868b9760c812ada
Pranav Agrawal <60977233+pranavagrawal321@users.noreply.github.com>
2022-11-30 02:25:39 +0530

    Create 1760A Medium Number.py

---
+++ 1760A Medium Number/1760A Medium Number.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    a, b, c = map(int, input().split())
+    if b < a < c or c < a < b:
+        print(a)
+    elif a < b < c or c < b < a:
+        print(b)
+    elif a < c < b or b < c < a:
+        print(c)
